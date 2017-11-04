fc57727edcc98bd9a880fcae2318a7db1408bd38
Vladimir Maksimenko <vl.maksime@gmail.com>
2017-11-04 08:24:01 +0300

    Добавил context.united.search.learn

---
+++ context.united.search.learn/context_learn.py
@@ -0,0 +1,19 @@
+﻿# -*- coding: utf-8 -*-
+# Module: context_learn
+# License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
+
+import xbmc
+
+from unitedsearch import UnitedSearch
+from unitedsearch import plugin
+
+_ = plugin.initialize_gettext()
+
+us = UnitedSearch()
+
+def main():
+    path = xbmc.getInfoLabel('ListItem.FileNameAndPath')
+    us.add_learned_addon(path)
+
+if __name__ == '__main__':
+    main()
